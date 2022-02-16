c0bb17d23d4fab9c3e865c85fe89b7fb52fbc52b
Jorge CP <64277389+jp2435@users.noreply.github.com>
2022-02-16 23:48:09 +0000

    Create servo.c

---
+++ servo.c
@@ -0,0 +1,35 @@
+// Exemplo de código usando uma placa de ensaico com um motor servo
+// C++ code
+//
+#include <Servo.h>
+
+int pos = 0;
+Servo servo_3;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  servo_3.attach(3, 500, 2500);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // sweep the servo from 0 to 180 degrees in steps
+  // of 1 degrees
+  for (pos = 0; pos <= 180; pos += 1) {
+    // tell servo to go to position in variable 'pos'
+    servo_3.write(pos);
+    // wait 15 ms for servo to reach the position
+    delay(15); // Wait for 15 millisecond(s)
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    // tell servo to go to position in variable 'pos'
+    servo_3.write(pos);
+    // wait 15 ms for servo to reach the position
+    delay(15); // Wait for 15 millisecond(s)
+  }
+}
