496054f0a00d424a067934a3d561464c2647b74f
Jorge CP <64277389+jp2435@users.noreply.github.com>
2022-02-17 16:45:26 +0000

    Update servo.cpp

--- servo.cpp
+++ servo.cpp
@@ -9,7 +9,7 @@ Servo servo_9;
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  servo_3.attach(9, 500, 2500);
+  servo_9.attach(9, 500, 2500);
 }
 
 void loop()
@@ -22,13 +22,13 @@ void loop()
   // of 1 degrees
   for (pos = 0; pos <= 180; pos += 1) {
     // tell servo to go to position in variable 'pos'
-    servo_3.write(pos);
+    servo_9.write(pos);
     // wait 15 ms for servo to reach the position
     delay(15); // Wait for 15 millisecond(s)
   }
   for (pos = 180; pos >= 0; pos -= 1) {
     // tell servo to go to position in variable 'pos'
-    servo_3.write(pos);
+    servo_9.write(pos);
     // wait 15 ms for servo to reach the position
     delay(15); // Wait for 15 millisecond(s)
   }
